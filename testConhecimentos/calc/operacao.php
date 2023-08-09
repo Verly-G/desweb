57b8c97bbb9aece298f370a28467a8a2e456bdc9
Gabriel <103848352+Verly-G@users.noreply.github.com>
2023-08-09 14:59:02 -0300

    .

--- testConhecimentos/calc/operacao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    function calculadora(float $n1, float $n2, string $operacao):float{
-        if($operacao === "+" ) return $n1 + $n2;
-        elseif($operacao === "-") return $n1 - $n2;
-        elseif($operacao === "*") return $n1 * $n2;
-        elseif($operacao === "/") return $n1 / $n2;
-    }
-?>
